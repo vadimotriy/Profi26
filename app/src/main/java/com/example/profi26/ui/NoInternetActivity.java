75b7d90d040bac200fb921b6a8a8a64fdef34592
Vadim <vblinov2009.rus@yandex.ru>
2026-03-01 23:08:04 +0300

    добавлена проверка на интернет

---
+++ app/src/main/java/com/example/profi26/ui/NoInternetActivity.java
@@ -0,0 +1,47 @@
+package com.example.profi26.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.profi26.R;
+
+public class NoInternetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Base_Theme_Profi26);
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_no_internet);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void checkAgain(View view) {
+        if (isOnline()) {
+            Intent onBoarding1 = new Intent(NoInternetActivity.this, OnBoarding1Activity.class);
+            startActivity(onBoarding1);
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+        }
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = manager.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnected();
+    }
+}
--- app/src/main/java/com/example/profi26/ui/OnBoarding1Activity.java
+++ app/src/main/java/com/example/profi26/ui/OnBoarding1Activity.java
@@ -1,11 +1,11 @@
 package com.example.profi26.ui;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +15,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.profi26.R;
 
+/* Класс OnBoarding1Activity
+ * Назначение: окрытие активности activity_on_boarding1
+ * Дата создания: 01.03.2026
+ * Автор создания: Блинов Вадим
+ */
 public class OnBoarding1Activity extends AppCompatActivity {
-
+    /* Метод onCreate
+     * Метод активируется при создании активности
+     * Метод открывает саму активность, меняет тему и проверяет наличие интренета
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.Base_Theme_Profi26);
+
+        if (!isOnline()) {
+            changeToNoInternet();
+        }
 
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -32,15 +44,34 @@ public class OnBoarding1Activity extends AppCompatActivity {
         });
     }
 
+    /* Метод changeActivity
+     * Метод активируется при нажатии на кнопку Next
+     * Метод служит для переход на активность activity_on_boarding2
+     */
     public void changeActivity(View view) {
         Intent onBoarding2 = new Intent(OnBoarding1Activity.this, OnBoarding2Activity.class);
         startActivity(onBoarding2);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 
+    /* Метод skip
+     * Метод активируется при нажатии на текст skip onboarding
+     * Метод служит для переход на активность activity_language_select
+     */
     public void skip(View view) {
         Intent languageSelect = new Intent(OnBoarding1Activity.this, LanguageSelectActivity.class);
         startActivity(languageSelect);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = manager.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnected();
+    }
+
+    public void changeToNoInternet() {
+        Intent noInternet = new Intent(OnBoarding1Activity.this, NoInternetActivity.class);
+        startActivity(noInternet);
     }
 }
